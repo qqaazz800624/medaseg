f2aeda88dfbe464e2f0210353fb11a2be9f01155
Pochuan Wang <d08922016@csie.ntu.edu.tw>
2021-12-17 16:05:52 +0800

    Add experimental distillation learning

--- manafaln/workflow/__init__.py
+++ manafaln/workflow/__init__.py
@@ -1,3 +1,4 @@
 from .base import SupervisedLearning
+from distillation import DistillationLearning
 from .segmentation import SupervisedSegmentation
 from .classification import SupervisedClassification
---
+++ manafaln/workflow/distillation.py
@@ -0,0 +1,55 @@
+import torch
+from manafaln.workflow import SupervisedLearning
+from manafaln.utils.builders import build_loss_fn
+
+class DistillationLearning(SupervisedLearning):
+    def __init__(self, config: dict):
+        super().__init__(config)
+
+        self.kd_lambda = config["settings"].get("kd_lambda", 1.0)
+
+        # Setup teacher model
+        self.teacher_model = torch.jit.load(config["settings"]["teacher_model_path"])
+        self.teacher_model.eval()
+
+        # Setup knowledge distillation loss
+        self.kd_loss_fn = build_loss_fn(config["settings"]["kd_loss"])
+
+    def training_step(self, batch, batch_idx):
+        image = batch["image"]
+        label = batch["label"]
+
+        # Model forward
+        batch["preds"] = self.model(image)
+
+        # Teacher model forward
+        with torch.no_grad():
+            kd_preds = self.teacher_model(image)
+            kd_preds = torch.nn.functional.softmax(kd_preds, dim=1)
+
+        # Compute losses
+        gt_loss = self.loss_fn(batch["preds"], batch["label"])
+        kd_loss = self.kd_loss_fn(batch["preds"], kd_preds)
+        loss = gt_loss + self.kd_lambda * kd_loss
+
+        if self.train_decollate is not None:
+            # Decolloate batch before post transform
+            for item in self.train_decollate(batch):
+                # Apply post transform on single item
+                item = self.post_transforms["training"](item)
+                # Compute metric for single item
+                self.train_metrics.apply(item)
+        else:
+            # Apply post transforms on the whole batch
+            batch = self.post_transforms["training"](batch)
+            # Add result of whole batch to metric
+            self.train_metrics.apply(batch)
+
+        # Log current loss value
+        self.log_dict({
+            "train_loss": loss,
+            "train_gt_loss": gt_loss,
+            "train_kd_loss": kd_loss
+        })
+
+        return loss
