4bd8874ba70fbb658e32b3283ad8deb2b9d390b4
Pochuan Wang <yukienanaha@gmail.com>
2022-01-05 16:39:20 +0800

    Update MultiEpochDataLoader

--- manafaln/utils/data.py
+++ manafaln/utils/data.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import DataLoader
+from monai.data import DataLoader
 
 class RepeatSampler(object):
     def __init__(self, sampler):
@@ -9,15 +9,16 @@ class RepeatSampler(object):
         while True:
             yield from iter(self.sampler)
 
-    def __len__(self):
-        return len(self.sampler)
-
 class MultiEpochDataLoader(DataLoader):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.batch_sampler = RepeatSampler(self.batch_sampler)
-        self.iterator = super().__init__()
+        # PyTorch 1.3 make batch_sampler private
+        object.__setattr__(self, 'batch_sampler', RepeatSampler(self.batch_sampler))
+        self.iterator = super().__iter__()
+
+    def __len__(self):
+        return len(self.batch_sampler.sampler)
 
     def __iter__(self):
         for i in range(len(self)):
