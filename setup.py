55cd6f6f58b52f25bac92068e0694c960cb230d0
Pochuan Wang <yukienanaha@gmail.com>
2022-12-05 16:19:34 +0800

    Fix an issue about low version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="manafaln",
-    version="0.3.0",
+    version="0.4.0",
     author="Pochuan Wang",
     author_email="d08922016@csie.ntu.edu.tw",
     packages=find_packages(),
