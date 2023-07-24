44e2637081d92838bd84f742a567d9fbaa0f7d5a
Pochuan Wang <yukienanaha@gmail.com>
2023-07-24 18:47:08 +0800

    Bugfix and add comments from ChatGPT.

--- manafaln/utils/codegen.py
+++ manafaln/utils/codegen.py
@@ -5,7 +5,25 @@ from importlib import import_module
 from manafaln.common.componentspecs import TransformSpec
 
 class ConfigParser(object):
+    """
+    A class responsible for parsing a configuration and extracting relevant information.
+
+    Attributes:
+        modules (List[Dict]): A list of dictionaries representing the modules.
+
+    Methods:
+        __init__(): Initializes the class and sets up the modules list.
+        parse(config: List[Dict]): Parses the configuration and returns a tuple containing the transforms list and imports dictionary.
+        __call__(config: List[Dict]): Calls the parse method with the given config parameter and returns its result.
+    """
     def __init__(self):
+        """
+        Initializes the class and sets up the modules list.
+        It iterates over a list of TransformSpec.PROVIDERS and creates a dictionary for each provider.
+        The dictionary contains two keys - "path" which stores the value of lib.TRANSFORM,
+        and "module" which imports the module specified by lib.TRANSFORM.
+        Each dictionary is then appended to the modules list.
+        """
         super().__init__()
 
         self.modules = []
@@ -17,6 +35,15 @@ class ConfigParser(object):
             self.modules.append(module)
 
     def parse(self, config: List[Dict]):
+        """
+        Parses the configuration and returns a tuple containing the transforms list and imports dictionary.
+
+        Args:
+            config (List[Dict]): A list of dictionaries representing the configuration.
+
+        Returns:
+            tuple: A tuple containing the transforms list and imports dictionary.
+        """
         imports = {}
         transforms = []        
 
@@ -32,7 +59,7 @@ class ConfigParser(object):
                         transform["path"] = m["path"]
                 if transform.get("path", None) is None:
                     raise RuntimeError(f"Unable to find transform {conf['name']}.")
-            transform["fullpath"] = transform["path"] + "." + transform["name"]
+            transform["fullpath"] = f"{transform['path']}.{transform['name']}"
             transform["args"] = conf.get("args", {})
 
             if transform["path"] in imports.keys():
@@ -44,10 +71,40 @@ class ConfigParser(object):
         return transforms, imports
 
     def __call__(self, config: List[Dict]):
+        """
+        Calls the parse method with the given config parameter and returns its result.
+
+        Args:
+            config (List[Dict]): A list of dictionaries representing the configuration.
+
+        Returns:
+            tuple: A tuple containing the transforms list and imports dictionary.
+        """
         return self.parse(config)
 
 class CodeGenerator(object):
+    """
+    A class that generates code based on a given configuration.
+
+    Methods:
+    - __init__(self, indent: int = 4, silent: bool = False): Initializes the CodeGenerator object with optional parameters indent and silent.
+    - write_with_indent(self, f: IO, code: str, level: int = 0): Writes the given code string to the file object f with optional indentation specified by the level parameter.
+    - write_import(self, imports: Dict[str, set], f: IO): Writes import statements to the file object f based on the imports dictionary.
+    - write_transform(self, transforms: List[Dict], f: IO): Writes a function definition and a series of transform calls to the file object f based on the transforms list.
+    - show_summary(self, transforms: List[Dict]): Generates a summary table of the transform names, paths, and arguments using the pandas and tabulate libraries. Prints the table to the console.
+    - generate_code(self, config: List[Dict], output: IO): Generates the code using the given configuration and writes it to the output file object. Calls write_import, write_transform, and show_summary methods.
+    """
     def __init__(self, indent: int = 4, silent: bool = False):
+        """
+        Initializes the CodeGenerator object.
+
+        Parameters:
+        - indent (int): The number of spaces used for indentation (default is 4).
+        - silent (bool): Flag to enable/disable console output (default is False).
+
+        Returns:
+        None
+        """
         super().__init__()
 
         self.indent = indent
@@ -55,14 +112,35 @@ class CodeGenerator(object):
         self.parser = ConfigParser()
 
     def write_with_indent(self, f: IO, code: str, level: int = 0):
+        """
+        Writes the given code string to the file object with optional indentation.
+
+        Parameters:
+        - f (IO): The file object to write the code to.
+        - code (str): The code string to write to the file object.
+        - level (int): The number of indentation levels (default is 0).
+
+        Returns:
+        None
+        """
         if level == 0:
             return f.write(code)
         return f.write(f"{'':>{self.indent * level}}{code}")
 
     def write_import(self, imports: Dict[str, set], f: IO):
+        """
+        Writes import statements to the file object based on the imports dictionary.
+
+        Parameters:
+        - imports (Dict[str, set]): A dictionary where the keys are the import statements and the values are the modules or packages to import.
+        - f (IO): The file object to write the import statements to.
+
+        Returns:
+        None
+        """
         for path, components in imports.items():
             if len(components) == 1:
-                c = next(iter(s))
+                c = next(iter(components))
                 self.write_with_indent(f, f"from {path} import {c}\n")
             else:
                 self.write_with_indent(f, f"from {path} import (\n")
@@ -72,11 +150,21 @@ class CodeGenerator(object):
         self.write_with_indent(f, "\n")
 
     def write_transform(self, transforms: List[Dict], f: IO):
+        """
+        Writes a function definition and a series of transform calls to the file object based on the transforms list.
+
+        Parameters:
+        - transforms (List[Dict]): A list of dictionaries where each dictionary represents a transform with keys 'name', 'path', and 'arguments'.
+        - f (IO): The file object to write the function definition and transform calls to.
+
+        Returns:
+        None
+        """
         self.write_with_indent(f, f"def get_transform():\n")
         self.write_with_indent(f, f"return Compose([\n", level=1)
 
         for t in transforms:
-            num_args = t["args"]
+            num_args = len(t["args"])
             if num_args == 0:
                 self.write_with_indent(f, f"{t['name']}(),\n", level=2)
             elif num_args == 1:
@@ -93,6 +181,16 @@ class CodeGenerator(object):
         self.write_with_indent(f, "])\n", level=1)
 
     def show_summary(self, transforms: List[Dict]):
+        """
+        Generates a summary table of the transform names, paths, and arguments using the pandas and tabulate libraries.
+        Prints the table to the console.
+
+        Parameters:
+        - transforms (List[Dict]): A list of dictionaries where each dictionary represents a transform with keys 'name', 'path', and 'arguments'.
+
+        Returns:
+        None
+        """
         try:
             import pandas as pd
             from tabulate import tabulate
@@ -107,7 +205,18 @@ class CodeGenerator(object):
         )
         print(tabulate(df, headers=headers, showindex="always", tablefmt="rst"))
 
-    def run(self, config: List[Dict], output: IO):
+    def generate_code(self, config: List[Dict], output: IO):
+        """
+        Generates the code using the given configuration and writes it to the output file object.
+        Calls the write_import, write_transform, and show_summary methods.
+
+        Parameters:
+        - config (List[Dict]): A list of dictionaries representing the configuration.
+        - output (IO): The file object to write the generated code to.
+
+        Returns:
+        None
+        """
         # Run analysis on given config file
         transforms, imports = self.parser(config)
 
@@ -141,7 +250,7 @@ if __name__ == "__main__":
     for e in entries:
         config = config[e]
 
-    gen = CodeGenerator(indent=args.indent, silent=args.silent)
+    generator = CodeGenerator(indent=args.indent, silent=args.silent)
     with open(args.output, "w") as of:
-        gen.run(config, of)
+        generator.generate_code(config, of)
 
