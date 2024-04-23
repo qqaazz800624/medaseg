28f345936e9a8e1fb5bcbe64463059d8b72ce2c5
Pochuan Wang <yukienanaha@gmail.com>
2024-04-23 09:03:17 +0000

    Add new MedNeXt implementation for segmentation task.

--- manafaln/models/__init__.py
+++ manafaln/models/__init__.py
@@ -1,4 +1,14 @@
 from .c2fnas import C2FNAS
 from .dynunet_ext import DynUNetExtended
+from .mednext import (
+    MedNeXt,
+    mednext_small,
+    mednext_base,
+    mednext_medium,
+    mednext_large
+)
 from .segclf import SegClfModel
-from .utils import FromTorchScript
+from .utils import (
+    FromTorchScript,
+    load_mednext_legacy_state_dict
+)
