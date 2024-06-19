516f8ad708756a4999260cc2dab7602a81564b75
Pochuan Wang <yukienanaha@gmail.com>
2024-06-19 17:38:17 +0800

    Add torchvision data module.

--- manafaln/data/__init__.py
+++ manafaln/data/__init__.py
@@ -2,3 +2,4 @@ from .dataset import HybridCacheDataset
 from .decathlon import DecathlonDataModule
 from .sampler import RatioSampler, SubsetRandomSampler, WeightedRandomSampler
 from .semi_supervised import SemiSupervisedDataModule
+from .torchvision import TorchVisionDataset, TorchVisionDataModule
