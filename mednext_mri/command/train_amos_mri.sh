ec7a927271bdaf13173d666892ce09642f228164
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-09 16:13:20 +0800

    update config and shell

--- mednext_mri/command/train_amos_mri.sh
+++ mednext_mri/command/train_amos_mri.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=1
+
+# Set default CUDA device to use
+DEFAULT_CUDA_DEVICE="2"
+
+# Allow overriding the default CUDA device with a command-line argument
+CUDA_DEVICE=${1:-$DEFAULT_CUDA_DEVICE}
+
+# Set the CUDA device to use
+export CUDA_VISIBLE_DEVICES=$CUDA_DEVICE
 
 python -m manafaln.apps.train --config config/config_train_amos_mri.yaml --seed 42 $@
