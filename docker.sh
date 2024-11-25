7dd08761e10ab503b0da875ee368292bd70b1a62
qqaazz800624 <qqaazz800624@gmail.com>
2024-11-25 17:21:33 +0800

    updates

--- docker.sh
+++ docker.sh
@@ -4,9 +4,12 @@ WORKDIR=$PWD
 
 docker run \
   -it --rm \
+  -u 1083:1091 \
   --ipc=host \
   --net=host \
+  --gpus=all \
   -v $WORKDIR/apps:/opt/monailabel/apps \
   -v $WORKDIR/datasets:/opt/monailabel/datasets \
   projectmonai/monailabel:latest \
-  /bin/bash
+  monailabel start_server --app apps/radiology --studies datasets/Task09_Spleen/imagesTr --conf models segmentation_spleen
+#  /bin/bash
