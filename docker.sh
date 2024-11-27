ee90a8ca4f7d3a1829f4fc703f5fd5416e220753
qqaazz800624 <qqaazz800624@gmail.com>
2024-11-27 16:44:16 +0800

    update docker shell

--- docker.sh
+++ docker.sh
@@ -11,5 +11,10 @@ docker run \
   -v $WORKDIR/apps:/opt/monailabel/apps \
   -v $WORKDIR/datasets:/opt/monailabel/datasets \
   projectmonai/monailabel:latest \
-  monailabel start_server --app apps/radiology --studies datasets/Task09_Spleen/imagesTr --conf models segmentation_spleen
+  monailabel start_server --app apps/radiology \
+  --studies datasets/Task09_Spleen/imagesTr \
+  --conf models segmentation_spleen \
+  --conf skip_scoring false \
+  --conf skip_strategies false \
+  --conf epistemic_enabled true
 #  /bin/bash
