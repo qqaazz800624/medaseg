6863c4b739d632c74cb0758a0650fd6040e85150
qqaazz800624 <qqaazz800624@gmail.com>
2025-04-09 19:13:29 +0800

    add pinkcc challeneg project

--- pinkcc/command/run_analysis.sh
+++ pinkcc/command/run_analysis.sh
@@ -4,4 +4,4 @@ python scripts/run_analysis.py \
     --datalist /neodata/open_dataset/PINKCC/datalist.json \
     --dataroot /neodata/open_dataset/PINKCC \
     --data_split_keys fold_0 fold_1 fold_2 fold_3 \
-    --output sctripts/analysis.json \
+    --output scripts/analysis.json \
--- pinkcc/command/train_pinkcc.sh
+++ pinkcc/command/train_pinkcc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set default CUDA device to use
-DEFAULT_CUDA_DEVICE="1"
+DEFAULT_CUDA_DEVICE="2"
 
 # Allow overriding the default CUDA device with a command-line argument
 CUDA_DEVICE=${1:-$DEFAULT_CUDA_DEVICE}
@@ -9,4 +9,4 @@ CUDA_DEVICE=${1:-$DEFAULT_CUDA_DEVICE}
 # Set the CUDA device to use
 export CUDA_VISIBLE_DEVICES=$CUDA_DEVICE
 
-python -m manafaln.apps.train --config config/config_train_amos_mri.yaml --seed 42 $@
+python -m manafaln.apps.train --config config/config_train_pinkcc.yaml --seed 42 $@
